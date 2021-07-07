56b287b6bfe3cfa5601cf93baee3ed98c265aa53
M7Mansour <mdmansour2000@gmail.com>
2021-07-07 20:08:05 +0300

    created footer for the two pages Relates #33

--- news/script.js
+++ news/script.js
@@ -3,19 +3,26 @@ let toggle1 = () => {
     if (nav1.className == "nav1")
         nav1.className += " responsive";
     else nav1.className = "nav1";
-}
+};
 
 function myFunction1(x) {
     x.classList.toggle("change1");
-}
+};
 
 let toggle2 = () => {
     const nav2 = document.body.querySelector(".nav2");
     if (nav2.className == "nav2")
         nav2.className += " responsive";
     else nav2.className = "nav2";
-}
+};
 
 function myFunction2(x) {
     x.classList.toggle("change2");
-}
+};
+
+let displayYear = () => {
+    const year = new Date().getFullYear();
+    document.body.querySelector("#year").innerText = year;
+};
+
+displayYear();
