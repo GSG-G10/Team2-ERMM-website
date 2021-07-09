5b66cd38e2fb8e14b4ad57cb09cf1eb5b76f1865
ElhamFadel <elham2000Fadel@gmail.com>
2021-07-09 14:26:12 +0300

    formate code by use prettire extention

--- news/script.js
+++ news/script.js
@@ -1,28 +1,26 @@
 const toggle1 = () => {
-    const nav1 = document.body.querySelector(".nav1");
-    if (nav1.className == "nav1")
-        nav1.className += " responsive";
-    else nav1.className = "nav1";
+  const nav1 = document.body.querySelector(".nav1");
+  if (nav1.className == "nav1") nav1.className += " responsive";
+  else nav1.className = "nav1";
 };
 
 const myFunction1 = (x) => {
-    x.classList.toggle("change1");
+  x.classList.toggle("change1");
 };
 
-const  toggle2 = () => {
-    const nav2 = document.body.querySelector(".nav2");
-    if (nav2.className == "nav2")
-        nav2.className += " responsive";
-    else nav2.className = "nav2";
+const toggle2 = () => {
+  const nav2 = document.body.querySelector(".nav2");
+  if (nav2.className == "nav2") nav2.className += " responsive";
+  else nav2.className = "nav2";
 };
 
-const myFunction2 = (x) =>{
-    x.classList.toggle("change2");
+const myFunction2 = (x) => {
+  x.classList.toggle("change2");
 };
 
-const  displayYear = () => {
-    const year = new Date().getFullYear();
-    document.body.querySelector("#year").innerText = year;
+const displayYear = () => {
+  const year = new Date().getFullYear();
+  document.body.querySelector("#year").innerText = year;
 };
 
 displayYear();
